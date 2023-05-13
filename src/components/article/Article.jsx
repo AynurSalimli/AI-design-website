52ab6976ebb47d584992a5ddca14fe6370f28b11
unknown <salimliaynur@gmail.com>
2023-05-13 21:07:19 +0400

    finished

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './article.scss'
-const Article = () => {
+const Article = ({ imgUrl, date, text }) => {
   return (
-    <div className='article'>Article</div>
-  )
-}
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
 
 export default Article
